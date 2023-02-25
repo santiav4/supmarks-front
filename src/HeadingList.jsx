ca8dc036d067deb71c3057973c48916876078957
santiav4 <itune.eight@gmail.com>
2023-02-25 09:23:00 -0400

    Added path to list and home

--- src/HeadingList.jsx
+++ src/HeadingList.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import listLogo from "./assets/list-logo.png";
 
 const HeadingList = () => {
     return (
-        <div className=" flex bg-[#0169E8]">
-            <div>flecha</div>
-            <div>logo</div>
-            <select name="" id="">
-                <option value="home">Home Delivery</option>
+        <div className=" flex justify-between bg-[#0169E8] text-white">
+            <a className=" text-black" href="/">
+                <IoIosArrowBack size={30} />
+            </a>
+            <img width={150} src={listLogo} alt="" />
+            <select className="text-black outline-none" name="" id="">
+                <option className="" value="home" slected>
+                    Home Delivery
+                </option>
                 <option value="pickup">Pick-Up (Recogido)</option>
             </select>
             <div>
-                <input className=" outline-none" type="text" />
-                <button> x Locate me</button>
+                <input
+                    className=" outline-none"
+                    placeholder="Guayama, Puerto Rico"
+                    type="text"
+                />
+                <button className=" border border-white"> x Locate me</button>
             </div>
             <div>list view</div>
             <div>Search</div>
